401c25bc198ea5313f9fe6eb903c7160f9f8b1b5
RobertoPrete <robertoprete2001@gmail.com>
2026-02-12 10:56:11 +0100

    modificato il menù di navigazione come un elenco puntato

--- header.php
+++ header.php
@@ -27,21 +27,20 @@ else
 <!--Inizio del documento HTML e meta tag!-->
 <!DOCTYPE html>
 <html lang="it">
-<head>
-  <meta charset="UTF-8"> <!-- Specifica la codifica dei caratteri -->
-  <meta name="author" content="Roberto Prete">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Rende il sito responsive -->
-  <meta name="description" content="Pagina di intestazione della piattaforma ECOnnectify"> <!-- Descrizione del sito -->
-  <meta name="keywords" content="eco, economia circolare, sostenibilità, riuso, riciclo, aziende, artigiani"> <!-- Parole chiave -->
-  <title>ECOnnectify</title> <!-- Titolo della pagina -->
-  <link rel="stylesheet" href="style.css"> <!-- Collegamento al file CSS -->
-  <!--<link rel="icon" type="image/jpeg" href="img/poli.jpeg">!--> <!-- Inserisce la favicon -->
-</head>
-    <body>';
-
-    <!--Genera l'intestazione della pagina con il titolo del sito!-->
+    <head>
+        <meta charset="UTF-8"> <!-- Specifica la codifica dei caratteri -->
+        <meta name="author" content="Roberto Prete">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Rende il sito responsive -->
+        <meta name="description" content="Pagina di intestazione della piattaforma ECOnnectify"> <!-- Descrizione del sito -->
+        <meta name="keywords" content="eco, economia circolare, sostenibilità, riuso, riciclo, aziende, artigiani"> <!-- Parole chiave -->
+        <title>ECOnnectify</title> <!-- Titolo della pagina -->
+        <link rel="stylesheet" href="style.css"> <!-- Collegamento al file CSS -->
+        <!--<link rel="icon" type="image/jpeg" href="img/poli.jpeg">!--> <!-- Inserisce la favicon -->
+    </head>
+    <body>
+        <!--Genera l'intestazione della pagina con il titolo del sito!-->
         <header>
-                <h1><img id="logo" src="img/logo1.jpg" alt="ECOnnectify"></h1>
+                <h1><img id="logo" src="img/logo1.jpg" alt="logo ECOnnectify"></h1>
                 <p class='slogan'>"Dallo scarto al valore: il futuro è sostenibile."</p>
         </header>
         <?php
@@ -51,15 +50,19 @@ else
 
         <!--Genera il menu di navigazione con i link alle pagine principali del sito!-->
         <nav>
-            <a href='home.php'>HOME</a> | <!-- Link alla pagina principale -->
-            <a href='lista.php'>LISTA</a> | <!-- Link alla pagina con la lista dei materiali -->
-            <a href='offerta.php'>OFFERTA</a> | <!-- Link alla pagina per gestire le offerte -->
-            <!-- CONTROLLA TIPO UTENTE  -->
-            <a href='domanda.php'>DOMANDA</a> | <!-- Link alla pagina per gestire le domande -->
-            <a href='registrazione.php'>REGISTRA</a> | <!-- Link alla pagina di registrazione -->
-            <a href='login.php'>LOGIN</a> | <!-- Link alla pagina di login -->
-            <!--<a href='logout.php'>LOGOUT</a>  // io lo attiverei solo se l'utente è loggato -->
-        </nav><hr> // Aggiunge una linea orizzontale sotto il menu di navigazione
+            <ul>
+                <li><a href='home.php'>HOME</a> <!-- Link alla pagina principale --></li>
+                <li><a href='lista.php'>LISTA</a> <!-- Link alla pagina con la lista dei materiali --></li>
+                <li><a href='offerta.php'>OFFERTA</a> <!-- Link alla pagina per gestire le offerte --></li>
+                <li><a href='domanda.php'>DOMANDA</a> <!-- Link alla pagina per gestire le domande --></li>
+                <li><a href='registrazione.php'>REGISTRA</a> <!-- Link alla pagina di registrazione --></li>
+                <li><a href='login.php'>LOGIN</a><!-- Link alla pagina di login --></li>
+                <?php
+                    if ($utente!=="non loggato")
+                        echo "<li><a href='logout.php'>LOGOUT</a></li>"; // mostro l'opzione per fare il logout sono se è stato fatto il login
+                ?>
+            </ul>
+        </nav><hr> <!--Aggiunge una linea orizzontale sotto il menu di navigazione-->
 
         <!--Commento per un link opzionale alla pagina "CONFERMA", da riattivare se necessario
         <a href='conferma.php'>CONFERMA</a> |  per lo spazio CONFERMA, se dovesse essere necessario riaggiungerlo -->
