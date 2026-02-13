2be46a4e4aeb6bb774c783da501df007e4304ea9
RobertoPrete <robertoprete2001@gmail.com>
2026-02-13 08:49:34 +0100

    aggiunto il logout e la conferma del logout

--- header.php
+++ header.php
@@ -11,10 +11,10 @@ else
 
 // Recupera il credito dell'utente dalla sessione, se disponibile, altrimenti imposta 0.00
 //$credit = isset($_SESSION['credit']) ? $_SESSION['credit'] : 0.00;
-if (isset($_SESSION["saldo"]))
-    $saldo=$_SESSION["saldo"];
+if (isset($_SESSION["credito"]))
+    $credito=$_SESSION["credito"];
 else
-    $saldo=$_SESSION["saldo"]= 0.00;
+    $credito=$_SESSION["credito"]= 0.00;
 
 //$typeutente = isset($_SESSION['type_utente']) ? $_SESSION['type_utente'] : '';
 if (isset($_SESSION["tipoUtente"]))
@@ -36,13 +36,18 @@ else
         <title>ECOnnectify</title> <!-- Titolo della pagina -->
         <link rel="stylesheet" href="style.css"> <!-- Collegamento al file CSS -->
         <!--<link rel="icon" type="image/jpeg" href="img/poli.jpeg">!--> <!-- Inserisce la favicon -->
+        <script>
+            function confermaLogout() { 
+                return confirm('Sei sicuro di voler uscire?')
+            }
+        </script>
     </head>
     <body>
         <!--Genera l'intestazione della pagina con il titolo del sito!-->
         <header>
                 <?php
                 //Mostra il nome dell'utente e il credito disponibile in alto a destra
-                echo "<div style='text-align:right; font-size:0.9em;'>Utente: $utente | Credito: € " . number_format($saldo, 2) . "</div>";
+                echo "<div style='text-align:right; font-size:0.9em;'>Utente: $utente | Credito: € " . number_format($credito, 2) . "</div>";
                 ?>
                 <h1><img id="logo" src="img/logo1.jpg" alt="logo ECOnnectify"></h1>
                 <p class='slogan'>"Dallo scarto al valore: il futuro è sostenibile."</p>             
@@ -60,7 +65,7 @@ else
                 <li><a href='login.php'>LOGIN</a><!-- Link alla pagina di login --></li>
                 <?php
                     if ($utente!=="non loggato")
-                        echo "<li><a href='logout.php'>LOGOUT</a></li>"; // mostro l'opzione per fare il logout sono se è stato fatto il login
+                        echo "<li><a href='logout.php' onclick='return confermaLogout()'>LOGOUT</a></li>"; // mostro l'opzione per fare il logout sono se è stato fatto il login
                 ?>
             </ul>
         </nav> <!--Aggiunge una linea orizzontale sotto il menu di navigazione-->
