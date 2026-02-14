c6d8b9af8e8efee3e51249454155fa0557cad757
RobertoPrete <robertoprete2001@gmail.com>
2026-02-14 12:23:10 +0100

    modificato il logout eliminando non solo la sessione ma anche tutti i cookie

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+foreach ($_COOKIE as $nome => $valore) { // Cicla attraverso tutti i cookie presenti
+    setcookie($nome, "", time() - 3600, "/"); // e li elimina impostando una data di scadenza passata (1 ora fa) e il percorso "/"
+}
 session_destroy();
 header('Location: home.php');
 exit;
