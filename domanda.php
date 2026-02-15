18f74fe4f0fde725cbbfb381eb7f5c6e84811102
RobertoPrete <robertoprete2001@gmail.com>
2026-02-15 19:18:25 +0100

    modificato:   domanda.php

--- domanda.php
+++ domanda.php
@@ -45,7 +45,15 @@
         $stmt = $conn->prepare("SELECT ID, NOME, DESCRIZIONE, DATA, QUANTITA, COSTO FROM MATERIALI WHERE DATA >= ?");
         $filter_date = new DateTime($filter_date); // Crea un oggetto DateTime per formattare la data correttamente
         $stmt->bind_param("s", $filter_date->format('Y-m-d')); // Associa il filtro della data alla query
-    } else {
+    }else if(!preg_match('/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/', $filter_date) && $filter_date !== "") {
+        // Se è stato fornito un filtro per la data ma non è in un formato valido, mostra un messaggio di errore e reindirizza alla stessa pagina
+        echo "<script>
+            document.addEventListener('DOMContentLoaded', function() {
+                alert('Formato data non valido. Inserire la data nel formato aaaa-mm-gg.');
+                window.location.href = 'domanda.php';
+            });
+            </script>";
+    }else {
         $stmt = $conn->prepare("SELECT ID, NOME, DESCRIZIONE, DATA, QUANTITA, COSTO FROM MATERIALI");
     }
 
