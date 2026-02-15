fabb4dd55d7c6aa9f89055894b35df2e098ff7ca
RobertoPrete <robertoprete2001@gmail.com>
2026-02-15 18:30:01 +0100

    sistemata la data di nascita nella registrazione

--- registrazione.php
+++ registrazione.php
@@ -77,7 +77,7 @@
                     // Validazione dei dati personali
                     if (preg_match("/^[A-Za-z ]{4,14}$/", $name) &&
                         preg_match("/^[A-Za-z' ]{4,16}$/", $surname) &&
-                        preg_match("/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/", $birthdate) &&
+                        preg_match("/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/", $birthdate) &&
                         preg_match("/^\d+(\.\d{1,2})?$/", $credit) &&
                         ((int)($credit * 100) % 5 === 0)) {
                         $stmtArtigiano = $conn->prepare("INSERT INTO DATI_ARTIGIANI (ID_UTENTE, NAME, SURNAME, BIRTHDATE, CREDIT, ADDRESS) VALUES (?, ?, ?, ?, ?, ?)");
@@ -89,8 +89,8 @@
                         $errors[] = "Nome non valido. Deve essere una stringa di minimo 4 e massimo 14 caratteri, con solo lettere ed il carattere spazio come caratteri accettabili.";
                     }else if (!preg_match("/^[A-Za-z' ]{4,16}$/", $surname)) {
                         $errors[] = "Cognome non valido. Deve essere una stringa di minimo 4 e massimo 16 caratteri, con solo lettere ed i caratteri spazio o “’” (apostrofo) come caratteri accettabili.";
-                    }else if (!preg_match("/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/", $birthdate)) {
-                        $errors[] = "Data di nascita non valida. Deve essere nella forma “aaaa-mm-gg” (dove il valore 0 in posizione più significativa nel mese e nel giorno può eventualmente essere omesso).";
+                    }else if (!preg_match("/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/", $birthdate)) {
+                        $errors[] = "Data di nascita non valida. Deve essere nella forma “aaaa-mm-gg”.";
                     }else if (!preg_match("/^\d+(\.\d{1,2})?$/", $credit) || ((int)($credit * 100) % 5 !== 0)) {
                         $errors[] = "Credito non valido. Deve essere un numero che rappresenta il credito in euro, caricato dagli utenti, con precisione dei centesimi (ma che nei centesimi deve considerare variazioni da 5 unità per volta).";
                     }else {
